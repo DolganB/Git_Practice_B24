7567fd928502c3b1d36f92ae8b59dd6d8af63bc5
dolgan <galsanulan@mail.com>
2021-10-03 12:24:11 -0400

    Updated second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,17 +3,20 @@ package setup;
 public class Second {
 
     public void m(){
+        System.out.println("first method");
 
 
     }
 
     public void m(int a){
+        System.out.println("second method");
 
 
     }
 
     public void m(String s){
-        
+        System.out.println("third method");
+
     }
 
 }
